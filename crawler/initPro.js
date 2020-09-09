be309fa8459bae51fc77033fc7256ec463496731
ifocus <475984203@qq.com>
2020-09-09 10:23:06 +0800

    加入定时任务

--- crawler/initPro.js
+++ crawler/initPro.js
@@ -84,21 +84,5 @@ async function getClassify(page, index) {
 // "热水器",
 const categoryList = ["冰柜", "冰箱", "空调", "电视", "燃气灶","热水器", "消毒柜", "洗碗机", "洗衣机"]
 
-async function run() {
-    const len = categoryList.length;
-    for (var i = 0; i < len; i++) {
-        await getClassify(1, categoryList[i])
-    }
-}
-
-function clearFn(){
-    let clearTable = "truncate table product"
-    mysql.EXECUTE(clearTable,[],(err,result) => {
-        if(err) throw err;
-        console.log("删除成功");
-    })
-
-}
-
 getClassify(1, 0)
 
